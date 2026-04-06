96b21239978c160721273f9bce55e6aa77daee3d
Anusha M N <mnanusha0@gmail.com>
2026-04-06 11:08:55 +0530

    Update status message in JSON response

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_comments():
     response = requests.request("GET", url, headers=headers, data=payload)
     return jsonify({
         "data": response.json(),
-        "status": "success",
+        "status": "successfully yes",
         "status_code": 200
     })
 
